3c9125848badd2daf2b31b13f3cf7f6262d08ef7
Al Viro <viro@zeniv.linux.org.uk>
2017-07-03 00:01:49 -0400

    net: annotate ->poll() instances
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -436,7 +436,8 @@ bool tcp_peer_is_proven(struct request_sock *req, struct dst_entry *dst);
 void tcp_disable_fack(struct tcp_sock *tp);
 void tcp_close(struct sock *sk, long timeout);
 void tcp_init_sock(struct sock *sk);
-unsigned int tcp_poll(struct file *file, struct socket *sock,
+void tcp_init_transfer(struct sock *sk, int bpf_op);
+__poll_t tcp_poll(struct file *file, struct socket *sock,
 		      struct poll_table_struct *wait);
 int tcp_getsockopt(struct sock *sk, int level, int optname,
 		   char __user *optval, int __user *optlen);
