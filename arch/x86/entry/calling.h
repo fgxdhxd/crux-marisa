ab4de46e2e7fcb92a1403976160f41f3ed96f54b
Dominik Brodowski <linux@dominikbrodowski.net>
2018-04-05 11:53:06 +0200

    syscalls/x86: Extend register clearing on syscall entry to lower registers
    To reduce the chance that random user space content leaks down the call
    chain in registers, also clear lower registers on syscall entry:
    
    For 64-bit syscalls, extend the register clearing in PUSH_AND_CLEAR_REGS
    to %dx and %cx. This should not hurt at all, also on the other callers
    of that macro. We do not need to clear %rdi and %rsi for syscall entry,
    as those registers are used to pass the parameters to do_syscall_64().
    
    For the 32-bit compat syscalls, do_int80_syscall_32() and
    do_fast_syscall_32() each only take one parameter. Therefore, extend the
    register clearing to %dx, %cx, and %si in entry_SYSCALL_compat and
    entry_INT80_compat.
    
    Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Brian Gerst <brgerst@gmail.com>
    Cc: Denys Vlasenko <dvlasenk@redhat.com>
    Cc: H. Peter Anvin <hpa@zytor.com>
    Cc: Josh Poimboeuf <jpoimboe@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/20180405095307.3730-8-linux@dominikbrodowski.net
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/entry/calling.h
+++ arch/x86/entry/calling.h
@@ -109,7 +109,9 @@ For 32-bit we have the following conventions - kernel is built with
 	pushq   %rsi		/* pt_regs->si */
 	.endif
 	pushq	\rdx		/* pt_regs->dx */
+	xorl	%edx, %edx	/* nospec   dx */
 	pushq   %rcx		/* pt_regs->cx */
+	xorl	%ecx, %ecx	/* nospec   cx */
 	pushq   \rax		/* pt_regs->ax */
 	pushq   %r8		/* pt_regs->r8 */
 	pushq   %r9		/* pt_regs->r9 */
