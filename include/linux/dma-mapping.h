796209ead0dfd33a3a3ae878011d49ab32609b4c
Christoph Hellwig <hch@lst.de>
2018-05-09 06:53:49 +0200

    dma-mapping: move the NEED_DMA_MAP_STATE config symbol to lib/Kconfig
    This way we have one central definition of it, and user can select it as
    needed.  Note that we now also always select it when CONFIG_DMA_API_DEBUG
    is select, which fixes some incorrect checks in a few network drivers.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Anshuman Khandual <khandual@linux.vnet.ibm.com>

--- include/linux/dma-mapping.h
+++ include/linux/dma-mapping.h
@@ -940,7 +940,7 @@ static inline int dma_mmap_nonconsistent(struct device *dev,
 	return dma_mmap_attrs(dev, vma, cpu_addr, dma_addr, size, attrs);
 }
 
-#if defined(CONFIG_NEED_DMA_MAP_STATE) || defined(CONFIG_DMA_API_DEBUG)
+#ifdef CONFIG_NEED_DMA_MAP_STATE
 #define DEFINE_DMA_UNMAP_ADDR(ADDR_NAME)        dma_addr_t ADDR_NAME
 #define DEFINE_DMA_UNMAP_LEN(LEN_NAME)          __u32 LEN_NAME
 #define dma_unmap_addr(PTR, ADDR_NAME)           ((PTR)->ADDR_NAME)
