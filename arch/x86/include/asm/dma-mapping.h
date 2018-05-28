00cb77daacc6bd3ed6a26f8ea7d5cbf844f3542f
Christoph Hellwig <hch@lst.de>
2018-05-28 12:47:57 +0200

    x86/pci-dma: switch the VIA 32-bit DMA quirk to use the struct device flag
    Instead of globally disabling > 32bit DMA using the arch_dma_supported
    hook walk the PCI bus under the actually affected bridge and mark every
    device with the dma_32bit_limit flag.  This also gets rid of the
    arch_dma_supported hook entirely.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/include/asm/dma-mapping.h
+++ arch/x86/include/asm/dma-mapping.h
@@ -30,10 +30,14 @@ static inline const struct dma_map_ops *get_arch_dma_ops(struct bus_type *bus)
 	return dma_ops;
 }
 
+<<<<<<< HEAD
 int arch_dma_supported(struct device *dev, u64 mask);
 #define arch_dma_supported arch_dma_supported
 
 bool arch_dma_alloc_attrs(struct device **dev, gfp_t *gfp);
+=======
+bool arch_dma_alloc_attrs(struct device **dev);
+>>>>>>> 0ead51c3fbd1 (x86/pci-dma: switch the VIA 32-bit DMA quirk to use the struct device flag)
 #define arch_dma_alloc_attrs arch_dma_alloc_attrs
 
 <<<<<<< HEAD
