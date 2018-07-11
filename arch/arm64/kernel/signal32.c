ba9f1432add9f80b47bbebd9e14c556e21103760
Mark Rutland <mark.rutland@arm.com>
2018-07-11 14:56:53 +0100

    arm64: use {COMPAT,}SYSCALL_DEFINE0 for sigreturn
    We don't currently annotate our various sigreturn functions as syscalls,
    as we need to do to use pt_regs syscall wrappers.
    
    Let's mark them as real syscalls.
    
    For compat_sys_sigreturn and compat_sys_rt_sigreturn, this changes the
    return type from int to long, matching the prototypes in sys32.c.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Reviewed-by: Dominik Brodowski <linux@dominikbrodowski.net>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/kernel/signal32.c
+++ arch/arm64/kernel/signal32.c
@@ -363,7 +363,7 @@ static int compat_restore_sigframe(struct pt_regs *regs,
 	return err;
 }
 
-asmlinkage int compat_sys_sigreturn(void)
+COMPAT_SYSCALL_DEFINE0(sigreturn)
 {
 	struct pt_regs *regs = current_pt_regs();
 	struct compat_sigframe __user *frame;
@@ -398,7 +398,7 @@ badframe:
 	return 0;
 }
 
-asmlinkage int compat_sys_rt_sigreturn(void)
+COMPAT_SYSCALL_DEFINE0(rt_sigreturn)
 {
 	struct pt_regs *regs = current_pt_regs();
 	struct compat_rt_sigframe __user *frame;
