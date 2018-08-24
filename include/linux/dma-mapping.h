8f7549db8cf3d13b9c6ca3039ba9d8c0e8d383c3
Christoph Hellwig <hch@lst.de>
2018-08-24 09:40:24 +0200

    dma-mapping: remove dma_configure
    There is no good reason for this indirection given that the method
    always exists.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Robin Murphy <robin.murphy@arm.com>

--- include/linux/dma-mapping.h
+++ include/linux/dma-mapping.h
@@ -843,14 +843,8 @@ static inline unsigned long dma_get_size(struct dma_coherent_mem *mem)
 #endif /* CONFIG_HAVE_GENERIC_DMA_COHERENT */
 
 #ifdef CONFIG_HAS_DMA
-int dma_configure(struct device *dev);
 void dma_deconfigure(struct device *dev);
 #else
-static inline int dma_configure(struct device *dev)
-{
-	return 0;
-}
-
 static inline void dma_deconfigure(struct device *dev) {}
 #endif
 
