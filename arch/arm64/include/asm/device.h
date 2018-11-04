d7ff77b3877abc6a44288d0009785351f59737c5
Christoph Hellwig <hch@lst.de>
2018-11-04 20:29:28 +0100

    dma-mapping: move the arm64 noncoherent alloc/free support to common code
    The arm64 codebase to implement coherent dma allocation for architectures
    with non-coherent DMA is a good start for a generic implementation, given
    that is uses the generic remap helpers, provides the atomic pool for
    allocations that can't sleep and still is realtively simple and well
    tested.  Move it to kernel/dma and allow architectures to opt into it
    using a config symbol.  Architectures just need to provide a new
    arch_dma_prep_coherent helper to writeback an invalidate the caches
    for any memory that gets remapped for uncached access.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Will Deacon <will.deacon@arm.com>
    Reviewed-by: Robin Murphy <robin.murphy@arm.com>

--- arch/arm64/include/asm/device.h
+++ arch/arm64/include/asm/device.h
@@ -23,7 +23,6 @@ struct dev_archdata {
 #ifdef CONFIG_XEN
 	const struct dma_map_ops *dev_dma_ops;
 #endif
-	bool dma_coherent;
 #ifdef CONFIG_ARM64_DMA_USE_IOMMU
 	struct dma_iommu_mapping       *mapping;
 #endif
