74785ea02217f947dc8d3242bc56be733f68aebf
Gustavo A. R. Silva <gustavoars@kernel.org>
2020-06-08 20:22:33 -0500

    overflow.h: Add flex_array_size() helper
    commit b19d57d0f3cc6f1022edf94daf1d70506a09e3c2 upstream.
    
    Add flex_array_size() helper for the calculation of the size, in bytes,
    of a flexible array member contained within an enclosing structure.
    
    Example of usage:
    
    struct something {
    	size_t count;
    	struct foo items[];
    };
    
    struct something *instance;
    
    instance = kmalloc(struct_size(instance, items, count), GFP_KERNEL);
    instance->count = count;
    memcpy(instance->items, src, flex_array_size(instance, items, instance->count));
    
    The helper returns SIZE_MAX on overflow instead of wrapping around.
    
    Additionally replaces parameter "n" with "count" in struct_size() helper
    for greater clarity and unification.
    
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>
    Link: https://lore.kernel.org/r/20200609012233.GA3371@embeddedor
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/overflow.h
+++ include/linux/overflow.h
@@ -357,6 +357,22 @@ static inline __must_check size_t __ab_c_size(size_t a, size_t b, size_t c)
 }
 
 /**
+ * struct_size() - Calculate size of structure with trailing array.
+ * @p: Pointer to the structure.
+ * @member: Name of the array member.
+ * @count: Number of elements in the array.
+ *
+ * Calculates size of memory needed for structure @p followed by an
+ * array of @count number of @member elements.
+ *
+ * Return: number of bytes needed or SIZE_MAX on overflow.
+ */
+#define struct_size(p, member, count)					\
+	__ab_c_size(count,						\
+		    sizeof(*(p)->member) + __must_be_array((p)->member),\
+		    sizeof(*(p)))
+
+/**
  * flex_array_size() - Calculate size of a flexible array member
  *                     within an enclosing structure.
  *
@@ -370,22 +386,7 @@ static inline __must_check size_t __ab_c_size(size_t a, size_t b, size_t c)
  * Return: number of bytes needed or SIZE_MAX on overflow.
  */
 #define flex_array_size(p, member, count)				\
-	size_mul(count,							\
-		 sizeof(*(p)->member) + __must_be_array((p)->member))
-
-/**
- * struct_size() - Calculate size of structure with trailing flexible array.
- *
- * @p: Pointer to the structure.
- * @member: Name of the array member.
- * @count: Number of elements in the array.
- *
- * Calculates size of memory needed for structure @p followed by an
- * array of @count number of @member elements.
- *
- * Return: number of bytes needed or SIZE_MAX on overflow.
- */
-#define struct_size(p, member, count)					\
-	size_add(sizeof(*(p)), flex_array_size(p, member, count))
+	array_size(count,						\
+		    sizeof(*(p)->member) + __must_be_array((p)->member))
 
 #endif /* __LINUX_OVERFLOW_H */
