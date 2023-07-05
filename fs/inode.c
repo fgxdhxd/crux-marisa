5b4e86c3b289c16f5da0b28d1ad60a010556465f
Jeff Layton <jlayton@kernel.org>
2023-07-05 14:58:10 -0400

    fs: add ctime accessors infrastructure
    struct timespec64 has unused bits in the tv_nsec field that can be used
    for other purposes. In future patches, we're going to change how the
    inode->i_ctime is accessed in certain inodes in order to make use of
    them. In order to do that safely though, we'll need to eradicate raw
    accesses of the inode->i_ctime field from the kernel.
    
    Add new accessor functions for the ctime that we use to replace them.
    
    Reviewed-by: Jan Kara <jack@suse.cz>
    Reviewed-by: Luis Chamberlain <mcgrof@kernel.org>
    Signed-off-by: Jeff Layton <jlayton@kernel.org>
    Reviewed-by: Damien Le Moal <dlemoal@kernel.org>
    Message-Id: <20230705185812.579118-2-jlayton@kernel.org>
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- fs/inode.c
+++ fs/inode.c
@@ -2173,6 +2173,22 @@ struct timespec current_time(struct inode *inode)
 }
 EXPORT_SYMBOL(current_time);
 
+/**
+ * inode_set_ctime_current - set the ctime to current_time
+ * @inode: inode
+ *
+ * Set the inode->i_ctime to the current value for the inode. Returns
+ * the current value that was assigned to i_ctime.
+ */
+struct timespec64 inode_set_ctime_current(struct inode *inode)
+{
+	struct timespec64 now = current_time(inode);
+
+	inode_set_ctime(inode, now.tv_sec, now.tv_nsec);
+	return now;
+}
+EXPORT_SYMBOL(inode_set_ctime_current);
+
 /*
  * Generic function to check FS_IOC_SETFLAGS values and reject any invalid
  * configurations.
