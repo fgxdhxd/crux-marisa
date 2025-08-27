768d979038248448fea54d26544f0d5c72ce8aa4
DawnT112 <3193782263@qq.com>
2025-08-27 19:24:05 +0800

    Import Micode Fingerprint

--- drivers/input/fingerprint/goodix_ta_capa/platform.c
+++ drivers/input/fingerprint/goodix_ta_capa/platform.c
@@ -3,9 +3,6 @@
  *
  * Coypritht (c) 2017 Goodix
  */
-#define DEBUG
-#define pr_fmt(fmt)     "gf_platform: " fmt
-
 #include <linux/delay.h>
 #include <linux/workqueue.h>
 #include <linux/of_gpio.h>
@@ -27,66 +24,58 @@ int gf_parse_dts(struct gf_dev *gf_dev)
 {
 #ifdef GF_PW_CTL
 	int rc = 0;
-	/*get pwr resource*/
-	gf_dev->pwr_gpio = of_get_named_gpio(gf_dev->spi->dev.of_node, "fp-gpio-pwr",
-					     0);
-
-	if (!gpio_is_valid(gf_dev->pwr_gpio)) {
-		pr_info("PWR GPIO is invalid.\n");
-		return -EPERM;
-	}
-
-	rc = gpio_request(gf_dev->pwr_gpio, "goodix_pwr");
-
-	if (rc) {
-		dev_err(&gf_dev->spi->dev, "Failed to request PWR GPIO. rc = %d\n", rc);
-		return -EPERM;
-	}
-
+       /*get pwr resource*/
+       gf_dev->pwr_gpio = of_get_named_gpio(gf_dev->spi->dev.of_node,"fp-gpio-pwr",0);
+       if (!gpio_is_valid(gf_dev->pwr_gpio)) {
+               pr_info("PWR GPIO is invalid.\n");
+               return -EPERM;
+       }
+       rc = gpio_request(gf_dev->pwr_gpio, "goodix_pwr");
+       if (rc) {
+               dev_err(&gf_dev->spi->dev, "Failed to request PWR GPIO. rc = %d\n", rc);
+               return -EPERM;
+       }
 #endif
-	/*get reset resource*/
-	gf_dev->reset_gpio = of_get_named_gpio(gf_dev->spi->dev.of_node,
-					       "goodix,gpio-reset", 0);
 
+	/*get reset resource*/
+	gf_dev->reset_gpio = of_get_named_gpio(gf_dev->spi->dev.of_node, "goodix,gpio-reset", 0);
 	if (!gpio_is_valid(gf_dev->reset_gpio)) {
 		pr_info("RESET GPIO is invalid.\n");
 		return -EPERM;
 	}
 
 	/*get irq resourece*/
-	gf_dev->irq_gpio = of_get_named_gpio(gf_dev->spi->dev.of_node,
-					     "goodix,gpio-irq", 0);
+	gf_dev->irq_gpio = of_get_named_gpio(gf_dev->spi->dev.of_node, "goodix,gpio-irq", 0);
 	pr_info("gf::irq_gpio:%d\n", gf_dev->irq_gpio);
-
 	if (!gpio_is_valid(gf_dev->irq_gpio)) {
 		pr_info("IRQ GPIO is invalid.\n");
 		return -EPERM;
 	}
 
+#ifdef GF_PW_CTL
+
+	gpio_direction_output(gf_dev->pwr_gpio, 1);
+	pr_info("goodix output pwr_gpio 1.\n");
+#endif
 	return 0;
 }
 
 void gf_cleanup(struct gf_dev *gf_dev)
 {
 	pr_info("[info] %s\n", __func__);
-
 	if (gpio_is_valid(gf_dev->irq_gpio)) {
 		gpio_free(gf_dev->irq_gpio);
 		pr_info("remove irq_gpio success\n");
 	}
-
 	if (gpio_is_valid(gf_dev->reset_gpio)) {
 		gpio_free(gf_dev->reset_gpio);
 		pr_info("remove reset_gpio success\n");
 	}
-
 #ifdef GF_PW_CTL
-
-	if (gpio_is_valid(gf_dev->pwr_gpio)) {
-		gpio_free(gf_dev->pwr_gpio);
-		pr_info("remove pwr_gpio success\n");
-	}
-
+       if (gpio_is_valid(gf_dev->pwr_gpio)) {
+               gpio_free(gf_dev->pwr_gpio);
+               pr_info("remove pwr_gpio success\n");
+       }
 #endif
 }
 
@@ -94,16 +83,13 @@ int gf_power_on(struct gf_dev *gf_dev)
 {
 	int rc = 0;
 #ifdef GF_PW_CTL
-
-	if (gpio_is_valid(gf_dev->pwr_gpio)) {
-		rc = gpio_direction_output(gf_dev->pwr_gpio, 1);
-		pr_info("---- power on result: %d----\n", rc);
-	} else {
-		pr_info("%s: gpio_is_invalid\n", __func__);
-	}
-
+        if (gpio_is_valid(gf_dev->pwr_gpio)) {
+                gpio_set_value(gf_dev->pwr_gpio, 1);
+        }
 #endif
 	msleep(10);
+	pr_info("---- power on ok ----\n");
+
 	return rc;
 }
 
@@ -111,15 +97,11 @@ int gf_power_off(struct gf_dev *gf_dev)
 {
 	int rc = 0;
 #ifdef GF_PW_CTL
-
-	if (gpio_is_valid(gf_dev->pwr_gpio)) {
-		rc = gpio_direction_output(gf_dev->pwr_gpio, 0);
-		pr_info("---- power off result: %d----\n", rc);
-	} else {
-		pr_info("%s: gpio_is_invalid\n", __func__);
-	}
-
+        if (gpio_is_valid(gf_dev->pwr_gpio)) {
+                gpio_set_value(gf_dev->pwr_gpio, 1);
+        }
 #endif
+	pr_info("---- power off ----\n");
 	return rc;
 }
 
@@ -129,12 +111,11 @@ int gf_hw_reset(struct gf_dev *gf_dev, unsigned int delay_ms)
 		pr_info("Input buff is NULL.\n");
 		return -EPERM;
 	}
-
 	gpio_direction_output(gf_dev->reset_gpio, 0);
+	gpio_set_value(gf_dev->reset_gpio, 0);
 	mdelay(3);
 	gpio_set_value(gf_dev->reset_gpio, 1);
 	mdelay(delay_ms);
-	pr_info("%s\n", __func__);
 	return 0;
 }
 
