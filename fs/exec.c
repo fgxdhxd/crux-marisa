d37b62802baee38bf33ddbddb690a93478cce186
wjy1214 <wjy1065676928@gmail.com>
2025-08-31 12:24:18 -0700

    try sukisu

--- fs/exec.c
+++ fs/exec.c
@@ -72,6 +72,14 @@
 
 #include <trace/events/sched.h>
 
+#ifdef CONFIG_KSU
+extern bool ksu_execveat_hook __read_mostly;
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+				 void *argv, void *envp, int *flags);
+#endif
+
 int suid_dumpable = 0;
 
 static LIST_HEAD(formats);
@@ -1711,6 +1719,13 @@ static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr envp,
 			      int flags)
 {
+#ifdef CONFIG_KSU
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	else
+		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+#endif
+
 	char *pathbuf = NULL;
 	struct linux_binprm *bprm;
 	struct file *file;
