eb8b6a5d169d10ffa19ad1dad331cb75dd2cfea1
wjy1214 <wjy1065676928@gmail.com>
2026-01-31 20:36:04 +0800

    move asm/unaligned.h to linux/unaligned.h asm/unaligned.h is always an include of asm-generic/unaligned.h; might as well move that thing to linux/unaligned.h and include that - there's nothing arch-specific in that header.
    auto-generated by the following:
    
    for i in `git grep -l -w asm/unaligned.h`; do
    	sed -i -e "s/asm\/unaligned.h/linux\/unaligned.h/" $i
    done
    for i in `git grep -l -w asm-generic/unaligned.h`; do
    	sed -i -e "s/asm-generic\/unaligned.h/linux\/unaligned.h/" $i
    done
    git mv include/asm-generic/unaligned.h include/linux/unaligned.h
    git mv tools/include/asm-generic/unaligned.h tools/include/linux/unaligned.h
    sed -i -e "/unaligned.h/d" include/asm-generic/Kbuild
    sed -i -e "s/__ASM_GENERIC/__LINUX/" include/linux/unaligned.h tools/include/linux/unaligned.h

--- fs/btrfs/disk-io.c
+++ fs/btrfs/disk-io.c
@@ -31,7 +31,7 @@
 #include <linux/uuid.h>
 #include <linux/semaphore.h>
 #include <linux/error-injection.h>
-#include <asm/unaligned.h>
+#include <linux/unaligned.h>
 #include "ctree.h"
 #include "disk-io.h"
 #include "hash.h"
