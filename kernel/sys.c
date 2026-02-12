1420634eee43a172ac28e8b88de1938e075b0743
wjy1214 <wjy1065676928@gmail.com>
2026-02-13 03:32:29 +0800

    Revert "使用ReSukiSU的hooks"
    This reverts commit 2d1ffe944fb49c30fd130f699120ba882d7186f9.

--- kernel/sys.c
+++ kernel/sys.c
@@ -590,9 +590,6 @@ error:
 	return retval;
 }
 
-#ifdef CONFIG_KSU_MANUAL_HOOK
-extern int ksu_handle_setresuid(uid_t ruid, uid_t euid, uid_t suid);
-#endif
 
 /*
  * This function implements a generic ability to update ruid, euid,
@@ -605,10 +602,6 @@ SYSCALL_DEFINE3(setresuid, uid_t, ruid, uid_t, euid, uid_t, suid)
 	struct cred *new;
 	int retval;
 	kuid_t kruid, keuid, ksuid;
-
-#ifdef CONFIG_KSU_MANUAL_HOOK
-       (void)ksu_handle_setresuid(ruid, euid, suid);
-#endif
 
 	kruid = make_kuid(ns, ruid);
 	keuid = make_kuid(ns, euid);
