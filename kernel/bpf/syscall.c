8d2afce66a32a78df50b240402e47f47654b0d3b
wjy1214 <wjy1065676928@gmail.com>
2026-03-13 20:49:03 +0800

    Revert "bpf: Pretend to support ANY prog_type/attach_type"
    This reverts commit d25047446189cffd870732cc26b9107058044408.

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -1225,22 +1225,10 @@ static int bpf_prog_load(union bpf_attr *attr)
 			goto free_prog;
 	}
 
-#ifdef CONFIG_ANDROID_SPOOF_KERNEL_VERSION_FOR_BPF
-retry_find_prog_type:
-#endif
 	/* find program type: socket_filter vs tracing_filter */
 	err = find_prog_type(type, prog);
-	if (err < 0) {
-#ifdef CONFIG_ANDROID_SPOOF_KERNEL_VERSION_FOR_BPF
-		if (err == -EINVAL && type != BPF_PROG_TYPE_DUMMY) {
-			pr_err("Overriding type = %d to BPF_PROG_TYPE_DUMMY", type);
-
-			type = BPF_PROG_TYPE_DUMMY;
-			goto retry_find_prog_type;
-		}
-#endif
+	if (err < 0)
 		goto free_prog;
-	}
 
 	/* run eBPF verifier */
 	err = bpf_check(&prog, attr);
@@ -1404,12 +1392,7 @@ static int bpf_prog_attach(const union bpf_attr *attr)
 	case BPF_SK_SKB_STREAM_VERDICT:
 		return sockmap_get_from_fd(attr, true);
 	default:
-#ifdef CONFIG_ANDROID_SPOOF_KERNEL_VERSION_FOR_BPF
-		pr_err("Pretending to support BPF attach_type = %d", attr->attach_type);
-		return 0;
-#else
 		return -EINVAL;
-#endif
 	}
 
 	prog = bpf_prog_get_type(attr->attach_bpf_fd, ptype);
@@ -1483,12 +1466,7 @@ static int bpf_prog_detach(const union bpf_attr *attr)
 	case BPF_SK_SKB_STREAM_VERDICT:
 		return sockmap_get_from_fd(attr, false);
 	default:
-#ifdef CONFIG_ANDROID_SPOOF_KERNEL_VERSION_FOR_BPF
-		pr_err("Pretending to support detach for BPF attach_type = %d", attr->attach_type);
-		return 0;
-#else
 		return -EINVAL;
-#endif
 	}
 
 	cgrp = cgroup_get_from_fd(attr->target_fd);
