4b85c3029200eb6c195d6054f9e78c67df6de54b
wjy1214 <wjy1065676928@gmail.com>
2026-05-01 18:39:54 +0800

    添加zstdn算法

--- drivers/block/zram/zcomp.c
+++ drivers/block/zram/zcomp.c
@@ -45,6 +45,9 @@ static const char * const backends[] = {
 #if IS_ENABLED(CONFIG_CRYPTO_ZSTD)
  	"zstd",
 #endif
+#if IS_ENABLED(CONFIG_CRYPTO_ZSTDN)
+	"zstdn",
+#endif
 };
 
 static void zcomp_strm_free(struct zcomp_strm *zstrm)
